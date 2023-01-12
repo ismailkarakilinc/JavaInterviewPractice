309aeb2418814921122d871cb924a95f31a5863c
Ismail <krklnc38@gmail.com>
2023-01-12 14:53:01 -0500

    Added 2 Java codes

---
+++ src/Ismail/DivideWithoutOperator.java
@@ -0,0 +1,23 @@
+package Ismail;
+
+public class DivideWithoutOperator {
+
+    public static void main(String[] args) {
+
+        int num1 = 19;
+        int num2 = 4;
+
+        int result = 0 ;
+
+        for (int i = num2 ; i <= num1 ; i = i+num2){    // (initialization; condition; iteration)
+
+            result++;
+
+        }
+
+        System.out.println(result);
+
+
+    }
+}
+
