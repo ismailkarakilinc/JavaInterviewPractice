78ca2692a2fcb2d1b0dbb3893ab4e042cbd63047
Ali Can <ainanc@gmail.com>
2023-01-16 17:50:58 -0500

    Ternary

--- src/alican/task01/OddOrEven.java
+++ src/alican/task01/OddOrEven.java
@@ -1,6 +1,7 @@
 package alican.task01;
 
 public class OddOrEven {
+    //Write a method identifies num is odd or even
     public static void main(String[] args) {
         System.out.println(oddOrEven(11));
     }
