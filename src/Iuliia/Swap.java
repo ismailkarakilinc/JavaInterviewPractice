f4a70129aa18a84f110357e3abd8389d8a9f72fe
jule4ka881 <riddle88200@gmail.com>
2023-01-20 15:26:20 -0500

    added swap

---
+++ src/Iuliia/Swap.java
@@ -0,0 +1,21 @@
+package Iuliia;
+
+public class Swap {
+    public static void main(String[] args) {
+
+
+        int x = 10,
+                y = 15;
+
+        x = x + y; //25
+        y = x - y; //25-15=10
+        x = x - y; //25-10=15
+        System.out.println("x=" + y + "\ny=" + x);
+
+
+    }
+}
+/*
+Swap two variables without using a third variable
+ */
+
