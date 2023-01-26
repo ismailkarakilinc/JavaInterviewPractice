21404d7f1aba543b564f8dd8986ec34697c6cf8d
fatihalpozdemir <ozdemir.fatihalp@gmail.com>
2023-01-26 00:47:09 -0500

    Task 5 , first and second questions

---
+++ src/Fatih/week4/task1.java
@@ -0,0 +1,23 @@
+package Fatih.week4;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class task1 {
+    public static void main(String[] args) {
+
+String str = "AAABBCDD";
+
+        for (int i = 0; i < str.length() ; i++) {
+            int count = 0;
+            for (int j = 0; j < str.length() ; j++) {
+                if(str.charAt(i) == str.charAt(j)){
+                    count++;
+                }
+            }
+            }
+
+
+
+    }
+}
